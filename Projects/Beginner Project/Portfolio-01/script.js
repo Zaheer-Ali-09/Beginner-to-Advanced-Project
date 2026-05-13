3529d6e372eb9c21c3d24affef9c3fb9ee4ffb98
Zaheer Ali <zaheercode09@gmail.com>
2026-05-13 10:04:00 +0500

    Hero Section

--- Projects/Beginner Project/Portfolio-01/script.js
+++
@@ -1,9 +0,0 @@
-// console.log("HEllo JS");
-
-
-let menuBar = document.getElementById("barMenu");
-let navMenu = document.getElementById("navMenu");
-
-menuBar.addEventListener("click", function () {
-    navMenu.classList.toggle("showMenu");
-});
